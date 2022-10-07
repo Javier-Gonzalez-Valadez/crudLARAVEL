ac1ce631f71d866e07e468466c98f5d6501c7280
Javier-Gonzalez-Valadez <javiergonzalez01@aragon.unam.mx>
2022-10-07 13:30:39 -0500

    Agregrando CRUD completo

--- resources/views/empleado/form.blade.php
+++ resources/views/empleado/form.blade.php
@@ -1,16 +1,18 @@
-<!--Llave de seguridad que solicita LARAVEL-->
-    <h1>{{$modo}} empleado</h1>
-    @if(count($errors)>0)
-        <div class="alert alert-danger">
-            <ul>
-            @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
+<!--AGREGANDO ICONO-->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" 
+integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" 
+crossorigin="anonymous">
 
-            @endforeach
-            </ul>
-        </div>
-        
-    @endif
+<h1>{{$modo}} empleado</h1>
+@if(count($errors)>0)
+    <div class="alert alert-danger">
+        <ul>
+        @foreach($errors->all() as $error)
+           <li>{{ $error }}</li>
+        @endforeach
+        </ul>
+    </div>
+@endif
 <div class="form-group">
     @csrf 
     <input type="text" name="Nombre" id="Nombre" placeholder="Nombre"  
@@ -39,10 +41,15 @@
     @if(isset($empleado->Foto))
     <img width="200" src="{{ asset('storage'.'/'.$empleado->Foto) }}" alt=""><br/>
     @endif
+    
+    </br>
 
+    <button type="submit" class="btn btn-primary" value="{{ $modo }}">
+        <i class='fa fa-user-check'></i>
+    </button>
 
-    <input type="submit" class="btn btn-primary" value="{{ $modo }}">
-
-    <a href="{{ url('empleado/') }}" class="btn btn-warning" style="text-decoration:none;">Regresar</a>
+    <a href="{{ url('empleado/') }}" class="btn btn-warning" style="text-decoration:none;">
+        <i class='fa fa-arrow-left'></i>
+    </a>
 </div>
         
--- resources/views/empleado/index.blade.php
+++ resources/views/empleado/index.blade.php
@@ -1,3 +1,8 @@
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" 
+integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" 
+crossorigin="anonymous"> 
+
+
 @extends('layouts.app')
 
 @section('content')
@@ -6,7 +11,7 @@
 
         @if(Session::has('mensaje'))
 
-        <div class="alert alert-primary alert-dismissible fade show" role="alert">
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
             <strong>{{ Session::get('mensaje') }}</strong> 
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
           </div>
@@ -17,7 +22,9 @@
 
 
 
-    <a href="{{ url('empleado/create') }}" class="btn btn-success"  style="text-decoration:none;">Nuevo</a>
+<a href="{{ url('empleado/create') }}" class="btn btn-success"  style="text-decoration:none;">
+    <i class='fa fa-user-plus'></i>
+</a>
 
 <table class="table table-responsive" >
     
@@ -47,14 +54,14 @@
                 <td>{{ $empleado->Correo }}</td>
                 <td>
                     <form action="{{ url('/empleado/'.$empleado->id.'/edit') }}" method="GET" style="display:inline-block">
-                        <input type="submit" class="btn btn-primary" value="Editar"> 
+                        <button type="submit" class="btn btn-primary"><i class='fas fa-user-edit'></i></button>
                     </form>  
                     
                     <form action="{{ url('/empleado/'.$empleado->id) }}" method="post" style="display:inline-block">
                         @csrf
                         {{ method_field('DELETE') }}
-                        <input type="submit" onclick="return confirm('¿Quieres Borrar?')" 
-                        value="Borrar" class="btn btn-danger">
+                        <button type="submit" onclick="return confirm('¿Quieres Borrar?')" 
+                        value="Borrar" class="btn btn-danger"><i class='fas fa-trash-alt'></i></button>
                     </form>
                 </td>
                 <td></td>
@@ -64,5 +71,6 @@
 
 
  </table>
+ {!! $empleados->links() !!}
 </div>
 @endsection  
